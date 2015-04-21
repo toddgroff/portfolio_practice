77017eae10fa1290d16e8f0959d615a8aba5bb3f
Todd Groff <todd.groff@gmail.com>
2015-04-21 11:35:49 -0400

    redirecting build path for index.html

---
+++ dist/js/app.js
@@ -0,0 +1,13 @@
+// The namespace for this application
+var app = {};
+
+app.showInitialPage = function () {
+  document.body.insertAdjacentHTML(
+    'beforeend',
+    '<h2>TODO: Remove example.js</h2>' +
+    '<p>And change init.js to do whatevz you need</p>');
+};
+
+app.showInitialPage();
+
+//# sourceMappingURL=app.js.map
